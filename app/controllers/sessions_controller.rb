5b04c82f7cf116cad69b1f685f27934517758a1b
satokofuwa <96944825+satokofuwa@users.noreply.github.com>
2022-03-21 09:20:12 +0900

    sessionsコントローラー中身作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,29 @@
 class SessionsController < ApplicationController
+  skip_before_action :login_required, only: [:new, :create]
+    def new
+    end
+          
+     def create
+    if logged_in?
+      flash[:notice] = 'Already logged in'
+      redirect_to new_session_path
+    else
+      
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user.id)
+    else
+      flash.now[:danger] = 'ログインに失敗しました'
+      render :new
+    end
+  end
 end
+      
+        def destroy
+          session.delete(:user_id)
+          flash[:notice] = 'ログアウトしました'
+          redirect_to new_session_path
+        end   
+end
+
