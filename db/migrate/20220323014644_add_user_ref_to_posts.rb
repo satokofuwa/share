ae9c561a47f402a8ad0f9b7df0fab143454524f6
satokofuwa <96944825+satokofuwa@users.noreply.github.com>
2022-03-23 10:56:36 +0900

    postテーブルに外部リファレンスインデックスuser_id作成”

--- db/migrate/20220323014644_add_user_ref_to_posts.rb
+++ db/migrate/20220323014644_add_user_ref_to_posts.rb
@@ -1,5 +1,5 @@
 class AddUserRefToPosts < ActiveRecord::Migration[6.0]
   def change
-    add_column :posts, :user, :refences
+    add_reference :posts, :user, foreign_key: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_22_061049) do
+ActiveRecord::Schema.define(version: 2022_03_23_014644) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 2022_03_22_061049) do
     t.string "title"
     t.text "content"
     t.string "image"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -43,4 +45,5 @@ ActiveRecord::Schema.define(version: 2022_03_22_061049) do
 
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
+  add_foreign_key "posts", "users"
 end
