8a8e8f3f3c9c8bd84a770000b0a070d7b6b3492a
satokofuwa <96944825+satokofuwa@users.noreply.github.com>
2022-03-26 02:29:42 +0900

    config/root にroot sessions#new 設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+    root "sessions#new"
     resources :posts
     resources :users, only: [:new, :create, :show, :edit, :update]
     resources :sessions, only: [:new, :create, :destroy]
