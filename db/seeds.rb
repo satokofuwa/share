4273d409e5a261d8e1c3f76fbd3328a5fcb68a7d
satokofuwa <96944825+satokofuwa@users.noreply.github.com>
2022-03-25 23:31:11 +0900

    Seedデータ投入コード作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 50.times do |n|
     name = Faker::Games::Pokemon.name
     email = Faker::Internet.email
-    created_at = 
-    updated_at =
-    image =
+    created_at = Faker::Date
+    updated_at =Faker::Date
+    image =Faker::IDNumber.valid
     password = "password"
     User.create!(name: name,
                  email: email,
