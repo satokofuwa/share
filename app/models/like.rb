9b20ffdd9bd402d20e95f51cc7ef0176c9d31316
satokofuwa <96944825+satokofuwa@users.noreply.github.com>
2022-03-27 13:59:29 +0900

    model like よりmount imageuploder 記述削除

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,5 @@
 class Like < ApplicationRecord
-  mount_uploader :image, ImageUploader
+  #mount_uploader :image, ImageUploader
   belongs_to :user
   belongs_to :post
   validates_uniqueness_of :post_id, scope: :user_id
